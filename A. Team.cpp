b327435936a37ff8cad476970bf65c0f801e3b0a
Tushar Sinha <121822994+tusharsinha08@users.noreply.github.com>
2023-07-18 02:55:33 +0600

    brute force, greedy

---
+++ A. Team.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    int cnt = 0;
+    while(t--){
+        int a, b, c;
+        cin>>a>>b>>c;
+        if((a+b+c) >= 2)
+            cnt++;
+    }
+    cout<<cnt<<endl;
+}
